9711f865f05a1012d11d9395bd53d630c2659351
MaDDoGSliM <45622894+MaDDoGSliM@users.noreply.github.com>
2026-04-06 17:09:04 +0200

    Add location and lot number to spool info screen
    Maybe a search for lot / location can be useful

--- app/src/main/java/com/hexxotest/spoolcompanion/network/data/SpoolItem.kt
+++ app/src/main/java/com/hexxotest/spoolcompanion/network/data/SpoolItem.kt
@@ -16,5 +16,7 @@ data class SpoolItem(
     val spool_weight: Double = 0.0,
     val used_length: Double = 0.0,
     val used_weight: Double = 0.0,
-    val comment: String = ""
+    val comment: String? = null,
+    val lot_nr: String? = null,
+    val location: String? = null
 )
